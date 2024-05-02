c1f45280b86ae50838fa72ba14c2e4d8a966a279
Michael Testut <michael.testut@yahoo.com>
2024-05-01 20:47:33 -0700

    Add Source Folder

---
+++ Source/ReckersTaphouse/ReckersTaphouseGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ReckersTaphouseGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AReckersTaphouseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AReckersTaphouseGameMode();
+};
+
+
+
